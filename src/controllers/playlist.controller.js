56af39254c9bd9a9eaa1ddc460a6bf58b9294a8b
rajj02 <shubhamraj781@gmail.com>
2024-04-19 10:14:33 -0700

    tweet and playlist done

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,43 +3,236 @@ import {Playlist} from "../models/playlist.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { User } from "../models/user.model.js"
+import { Video } from "../models/video.model.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
     //TODO: create playlist
+
+    const createPlaylist = await Playlist.create({
+        name : name,
+        description: description,
+        owner : req.user,
+
+    })
+
+    if(!createPlaylist)
+    {
+        throw new ApiError(500,"something went wrong");
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,createPlaylist,"playlist created successfully")
+    )
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    const user = await User.findById(userId);
+
+    if(!user)
+    {
+        throw new ApiError(400,"user not found")
+    }
+
+    if(user._id.toString() !== req.user._id.toString())
+    {
+      throw new ApiError(400,"you are not authorized to see get the requested playlist")
+    }
+
+    const playlists = await Playlist.find({owner : userId})
+
+    if(!playlists)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,playlists,"playlists fetched successfully")
+    )
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    const playlistById = await Playlist.findById(playlistId);
+
+    if(!playlistById)
+    {
+     new ApiError(400,"playlist not found")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,playlistById,"playlist fetched successfully"))
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+
+    const playlist = await Playlist.findById(playlistId);
+    const video = await Video.findById(videoId)
+
+    if(!playlist)
+    {
+        throw new ApiError(400,"playlist not found")
+    }
+
+    if(!video)
+    {
+        throw new ApiError(400,"video not found")
+    }
+
+    const videoAdded = await Playlist.updateOne(
+        {_id : playlistId},
+        {
+            $push:{
+                videos: videoId
+            }
+        }
+    )
+
+    if(!videoAdded)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,videoAdded,"video added successfully")
+    )
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
 
+    const playlist = await Playlist.findById(playlistId);
+    const video = await Video.findById(videoId)
+
+    if(!playlist)
+    {
+        throw new ApiError(400,"playlist not found")
+    }
+
+    if(!video)
+    {
+        throw new ApiError(400,"video not found")
+    }
+
+    const videoRemoved = await Playlist.updateOne(
+        {_id : playlistId},
+        {
+            $pull:{
+                videos: videoId
+            }
+        }
+    )
+
+    if(!videoRemoved)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,videoRemoved,"video removed successfully")
+    )
+
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist)
+    {
+        throw new ApiError(400,"no playlist available");
+    }
+
+    const ownerOfPlaylist = playlist.owner;
+
+    if(!ownerOfPlaylist)
+    {
+        throw new ApiError(401,"you are not authorize to delete playlist")
+    }
+
+    if(ownerOfPlaylist.toString() !== req.user._id.toString())
+    {
+        throw new ApiError(401,"you are not authorize to delete playlist")
+    }
+
+    const playlistDeleted  = await Playlist.findOneAndDelete(playlistId);
+
+    if(!playlistDeleted)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,playlistDeleted,"palylist deleted successfully")
+    )
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist)
+    {
+        throw new ApiError(400,"no playlist available");
+    }
+
+    const ownerOfPlaylist = playlist.owner;
+
+    if(!ownerOfPlaylist)
+    {
+        throw new ApiError(401,"you are not authorize to update playlist")
+    }
+
+    if(ownerOfPlaylist.toString() !== req.user._id.toString())
+    {
+        throw new ApiError(401,"you are not authorize to update playlist")
+    }
+    
+    const playlistUpdated = await Playlist.findByIdAndUpdate(
+        playlistId,
+         {
+             $set:{
+                name : name,
+                description : description
+             }
+         },
+         {new: true}
+     )
+ 
+     if(!playlistUpdated)
+     {
+         throw new ApiError(500, "something went wrong")
+     }
+ 
+     return res
+     .status(201)
+     .json(
+         new ApiResponse(201, playlistUpdated,"playlist updated successfully")
+     )
 })
 
 export {
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,18 +7,152 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body;
+
+    if(!content)
+    {
+        throw new ApiError(400,"please provide the content of the tweet");
+    }
+
+    const tweetCreated = await Tweet.create({
+        content:content,
+        owner:req.user
+    })
+
+    if(!tweetCreated)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,tweetCreated,"tweet created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params;
+
+    if(!userId)
+    {
+        throw new ApiError(400,"please provide the userId");
+    }
+
+    // const tweets = await Tweet.aggregate(
+    //    [
+    //     {
+    //         $match:{
+    //         owner : new mongoose.Types.ObjectId(userId)
+    //         }
+    //     },
+    //     {
+    //         $lookup:{
+    //           from:"users",
+    //           localField:"owner",
+    //           foreignField:"_id",
+    //           as:"ownerOfTweets"
+    //         }
+    //     }
+    //    ]
+    // )
+    
+    const tweets = await Tweet.find({owner : userId})
+
+    if(!tweets)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,tweets,"tweets fetched succesfully")
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId} = req.params;
+    const {content} = req.body;
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet)
+    {
+        throw new ApiError(400,"no tweet available");
+    }
+
+    const ownerOfTweet = tweet.owner;
+
+    if(!ownerOfTweet)
+    {
+        throw new ApiError(401,"you are not authorize to delete tweet")
+    }
+
+    if(ownerOfTweet.toString() !== req.user._id.toString())
+    {
+        throw new ApiError(401,"you are not authorize to delete tweet")
+    }
+
+    const tweetUpdated = await Tweet.findByIdAndUpdate(
+        tweetId,
+         {
+             $set:{
+                 content: content
+             }
+         },
+         {new: true}
+     )
+ 
+     if(!tweetUpdated)
+     {
+         throw new ApiError(500, "something went wrong")
+     }
+ 
+     return res
+     .status(201)
+     .json(
+         new ApiResponse(201, tweetUpdated,"comment updated successfully")
+     )
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params;
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet)
+    {
+        throw new ApiError(400,"no tweet available");
+    }
+
+    const ownerOfTweet = tweet.owner;
+
+    if(!ownerOfTweet)
+    {
+        throw new ApiError(401,"you are not authorize to delete tweet")
+    }
+
+    if(ownerOfTweet.toString() !== req.user._id.toString())
+    {
+        throw new ApiError(401,"you are not authorize to delete tweet")
+    }
+
+    const tweetDeleted = await Tweet.findOneAndDelete(tweetId);
+
+    if(!tweetDeleted)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,tweetDeleted,"tweet deleted successfully")
+    )
 })
 
 export {
