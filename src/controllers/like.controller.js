fba025ab1c1d8a0716108faa2f51f60b166c87fa
rajj02 <shubhamraj781@gmail.com>
2024-04-21 00:50:00 -0700

    like and subscription controller done

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,26 +3,122 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {Video} from "../models/video.model.js"
+import {Comment} from "../models/comment.model.js"
+import {Tweet} from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    const video = await Video.findById(videoId)
+
+    if(!video)
+    {
+        throw new ApiError(400,"video not found")
+    }
+
+    const findLikedVideo = await Like.find({video : videoId})
+    console.log(findLikedVideo)
+    let videoToggled;
+    if(findLikedVideo?.length==0)
+    {
+        videoToggled = await Like.create({
+        video : videoId,
+        likedBy : video.owner
+       })
+    }
+    else if(findLikedVideo?.length != 0)
+    {
+        videoToggled = await Like.findOneAndDelete({video : videoId})
+    }
+   console.log(videoToggled)
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,videoToggled,"video toggled successfully")
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    const comment = await Comment.findById(commentId)
+
+    if(!comment)
+    {
+        throw new ApiError(400,"coment not found")
+    }
+
+    const findLikedComment = await Like.find({comment : commentId})
+    let commentToggled;
+    if(findLikedComment?.length == 0)
+    {
+        commentToggled = await Like.create({
+        comment : commentId,
+        likedBy : comment.owner
+       })
+    }
+    else if(findLikedComment?.length != 0)
+    {
+        commentToggled = await Like.findOneAndDelete({comment : commentId})
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,commentToggled,"comment toggled successfully")
+    )
+    
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet)
+    {
+        throw new ApiError(400,"tweet not found")
+    }
+
+    const findLikedTweet = await Like.find({tweet : tweetId})
+    let tweetToggled;
+    if(findLikedTweet?.length == 0)
+    {
+        tweetToggled = await Like.create({
+        tweet : tweetId,
+        likedBy : tweet.owner
+       })
+    }
+    else if(findLikedTweet?.length != 0)
+    {
+        tweetToggled = await Like.findOneAndDelete({tweet :tweetId})
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,tweetToggled,"tweet toggled successfully")
+    )
+    
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const allLikedVideos = await Like.find({likedBy : req.user._id,video: { $exists: true }});
+
+    if(!allLikedVideos)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(201,allLikedVideos,"liked videos fetched successfully")
+    )
 })
 
 export {
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,16 +9,90 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    const channel = await Subscription.find({channel : channelId})
+
+    if(!channel)
+    {
+        throw new ApiError(400,"channel not found")
+    }
+
+    const findSubscriber = await Subscription.find({subscriber : req.user._id,channel : channelId});
+    console.log(findSubscriber)
+
+    let toggleSubscriber;
+    if(findSubscriber?.length ==0)
+    {
+      toggleSubscriber = await Subscription.create({
+        subscriber : req.user._id,
+        channel : channelId
+      })
+    }
+
+    else if(findSubscriber?.length !=0)
+    {
+        toggleSubscriber = await Subscription.findOneAndDelete({subscriber : req.user._id,channel : channelId})
+    }
+
+    if(!toggleSubscriber)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,toggleSubscriber,"subscription toggled successfully")
+    )
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    const channel = await User.findById(channelId);
+
+    if(!channel)
+    {
+        throw new ApiError(400,"channel does not exist");
+    }
+
+    const getSubscribers = await Subscription.find({channel : channelId});
+
+    if(!getSubscribers)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,getSubscribers,"subscribers fetched successfully")
+    )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    const subscriber = await User.findById(subscriberId);
+
+    if(!subscriber)
+    {
+        throw new ApiError(400,"subscriber does not exist");
+    }
+
+    const getChannels = await Subscription.find({subscriber : subscriberId});
+
+    if(!getChannels)
+    {
+        throw new ApiError(500,"something went wrong")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,getChannels,"Channels fetched successfully")
+    )
 })
 
 export {
