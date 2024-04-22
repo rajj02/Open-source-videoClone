320f2921a594b418e921361d904ebd551a89ac08
rajj02 <shubhamraj781@gmail.com>
2024-04-22 07:19:40 -0700

    dashboard and health controller done

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,70 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+   
+   //finding number of videos added by channel
+    const videos = await Video.aggregate([
+       {
+         $match:{
+            owner : req.user._id
+        }
+       },
+       {
+        $group: {
+            _id: null,
+            count: { $sum: 1 } 
+        }
+       }
+        
+    ])
+
+    const allSubscribers = await Subscription.aggregate([
+        {
+            $match:{
+                channel : req.user._id
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                count: { $sum: 1 } 
+            }
+        }
+     ])
+    if(!videos)
+    {
+        throw new ApiError(400,"videos not available")
+    }
+    if(!allSubscribers)
+    {
+        throw new ApiError(400,"channel has no subscriber")
+    }
+  
+     const videoCount = videos.length > 0 ? videos[0].count : 0;
+     const subscriberCount = allSubscribers.length > 0 ? allSubscribers[0].count :0;
+     
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,`number of videos : ${videoCount} and number of subscribers : ${subscriberCount} fetched successfully`)
+    )
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const videos = await Video.find({owner : req.user._id});
+
+    if(!videos)
+    {
+        throw new ApiError(400,"videos not available")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,videos,"videos fetched successfully")
+    )
 })
 
 export {
--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -5,6 +5,18 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    const {healthStatus} = req.body;
+
+    if(!healthStatus)
+    {
+        throw ApiError(404,"health status not available")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,healthStatus,"health status fetched successfully")
+    )
 })
 
 export {
